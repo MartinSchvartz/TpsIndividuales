a2b349569feb42702eb6d8ed4694c549445b9228
MartinSchvartz <39829852+MartinSchvartz@users.noreply.github.com>
2021-07-02 08:40:41 -0300

    Update tpMuerteEnLaMansion.pl

--- tpMuerteEnLaMansion.pl
+++ tpMuerteEnLaMansion.pl
@@ -37,3 +37,14 @@ mata(Asesino,Victima):-
     vivenEnLaMansionDreadbury(Asesino),
     not(esMasRicoQueTiaAgatha(Asesino)).
 
+/* 1. El programa puede mostrar quien mato a Tia Agatha con la ultima consulta, mata(Asesino,Victima). Si hacemos la consulta como mata(Asesino,tiaAgatha),
+nos devuelve Asesino = charles.
+2. 
+-Alguien odia a milhouse? eso podemos resolverlo consultando odiadorOdiado(_,milhouse), lo cual ons devuelve un false.
+-A quien odia Charles?  haciendo la consulta charlesOdia(Quien), obtenermos como resultado que charles odia a tia agatha y al mayordomo
+Quien = tiaAgatha ;
+Quien = mayordomo.
+-El nombre de quien odia a tia agatha? esto se resuelve con la consulta odiadorOdiado(OdiaAAgatha, tiaAgatha). reotrnandonos OdiaAAgatha = charles, siendo asi que
+charles odia a tia Agatha.
+-Todos los odiadores y sus odiados se puede responder con la consulta odiadorOdiado(Odiador,Odiado). Lo cual nos dice quien odia a quien.
+-Para saber si el mayordomo odia a alguien simplemente consultamos mayordomoOdia(_). lo cual nos dice true ya que odia a charles
